40661aee46262ddfe7850b3e205a1470001ba61b
Vinicius Corbelli <v.corbelli71@gmail.com>
2021-07-17 17:57:10 -0300

    Ajustes do codigo para os arquivos enviado pelo professor

---
+++ Graph.cpp
@@ -0,0 +1,309 @@
+#include "Graph.h"
+#include "Node.h"
+#include "Edge.h"
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <list>
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+#include <float.h>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+/**************************************************************************************************
+ * Defining the Graph's methods
+**************************************************************************************************/
+
+// Constructor
+Graph::Graph(int order, bool directed, bool weighted_edge, bool weighted_node)
+{
+
+    this->order = order;
+    this->directed = directed;
+    this->weighted_edge = weighted_edge;
+    this->weighted_node = weighted_node;
+    this->first_node = this->last_node = nullptr;
+    this->number_edges = 0;
+}
+
+// Destructor
+Graph::~Graph()
+{
+
+    Node *next_node = this->first_node;
+
+    while (next_node != nullptr)
+    {
+
+        next_node->removeAllEdges();
+        Node *aux_node = next_node->getNextNode();
+        delete next_node;
+        next_node = aux_node;
+    }
+}
+
+// Getters
+int Graph::getOrder()
+{
+
+    return this->order;
+}
+int Graph::getNumberEdges()
+{
+
+    return this->number_edges;
+}
+//Function that verifies if the graph is directed
+bool Graph::getDirected()
+{
+
+    return this->directed;
+}
+//Function that verifies if the graph is weighted at the edges
+bool Graph::getWeightedEdge()
+{
+
+    return this->weighted_edge;
+}
+
+//Function that verifies if the graph is weighted at the nodes
+bool Graph::getWeightedNode()
+{
+
+    return this->weighted_node;
+}
+
+Node *Graph::getFirstNode()
+{
+
+    return this->first_node;
+}
+
+Node *Graph::getLastNode()
+{
+
+    return this->last_node;
+}
+
+bool Graph::hasNode(int id)
+{
+    if (this->first_node != nullptr)
+    {
+        for (Node *aux = this->first_node; aux != nullptr; aux = aux->getNextNode())
+            if (aux->getId() == id)
+                return true;
+    }
+
+    return false;
+}
+
+// Other methods
+/*
+    The outdegree attribute of nodes is used as a counter for the number of edges in the graph.
+    This allows the correct updating of the numbers of edges in the graph being directed or not.
+*/
+void Graph::insertNode(int id)
+{
+    if (!hasNode(id))
+    {
+        if (this->first_node != nullptr)
+        {
+            Node *no = new Node(id);
+            this->last_node->setNextNode(no);
+            this->last_node = no;
+        }
+        else
+        {
+            this->first_node = new Node(id);
+            this->last_node = this->first_node;
+        }
+    }
+}
+
+void Graph::insertEdge(int id, int target_id, float weight)
+{
+    if (!hasNode(id))
+        this->insertNode(id);
+
+    if (!hasNode(target_id))
+        this->insertNode(target_id);
+
+    if (this->directed)
+    {
+        Node *no = getNode(id);
+        Node *no_alvo = getNode(target_id);
+        no->insertEdge(target_id, weight);
+        no->incrementInDegree();
+        no_alvo->incrementOutDegree();
+    }
+    else
+    {
+        Node *no = getNode(id);
+        no->insertEdge(target_id, weight);
+        no->incrementInDegree();
+    }
+}
+
+void Graph::removeNode(int id)
+{
+    if (hasNode(id))
+    {
+        if (this->first_node->getId() == id)
+        {
+            Node *aux = this->first_node;
+            this->first_node = this->first_node->getNextNode();
+            delete aux;
+        }
+        else
+        {
+            Node *anterior;
+            Node *aux = this->first_node;
+
+            while (aux != nullptr)
+            {
+                if (aux->getId() == id)
+                {
+                    break;
+                }
+
+                anterior = aux;
+                aux = aux->getNextNode();
+            }
+
+            if (this->last_node == aux)
+            {
+                this->last_node = anterior;
+            }
+
+            anterior->setNextNode(aux->getNextNode());
+
+            Node *p = this->first_node;
+            while (p != nullptr)
+            {
+                p->removeEdge(id, directed, p);
+            }
+
+            delete aux;
+        }
+    }
+}
+
+bool Graph::searchNode(int id)
+{
+}
+
+Node *Graph::getNode(int id)
+{
+    if (this->first_node != nullptr)
+    {
+        for (Node *aux = this->first_node; aux != nullptr; aux = aux->getNextNode())
+            if (aux->getId() == id)
+                return aux;
+    }
+
+    return nullptr;
+}
+
+//Function that prints a set of edges belongs breadth tree
+
+void Graph::breadthFirstSearch(ofstream &output_file)
+{
+}
+
+float Graph::floydMarshall(int idSource, int idTarget)
+{
+}
+
+float Graph::dijkstra(int idSource, int idTarget)
+{
+}
+
+//function that prints a topological sorting
+void topologicalSorting()
+{
+}
+
+void breadthFirstSearch(ofstream &output_file)
+{
+}
+Graph *getVertexInduced(int *listIdNodes)
+{
+}
+
+Graph *agmKuskal()
+{
+}
+Graph *agmPrim()
+{
+}
+
+// Funções da primeira etapa
+
+void Graph::transitivoDireto(ofstream *arquivo_saida, int id)
+{
+    vector<int> no;
+
+    Node *node = getNode(id);
+
+    for (Edge *aresta = node->getFirstEdge(); aresta != nullptr; aresta = aresta->getNextEdge())
+    {
+        if (!std::count(no.begin(), no.end(), node->getId()))
+        {
+            no.push_back(aresta->getTargetId());
+            transitivoDireto_Aux(&no, getNode(aresta->getTargetId()));
+        }
+    }
+
+    for (int i = 0; i < no.size(); i++)
+    {
+        *arquivo_saida << no[i] << " ";
+    }
+}
+
+void Graph::transitivoDireto_Aux(vector<int> *no, Node *node)
+{
+    for (Edge *aresta = node->getFirstEdge(); aresta != nullptr; aresta = aresta->getNextEdge())
+    {
+        if (!std::count(no->begin(), no->end(), aresta->getTargetId()))
+        {
+            no->push_back(aresta->getTargetId());
+            transitivoDireto_Aux(no, getNode(aresta->getTargetId()));
+        }
+    }
+}
+
+void Graph::transitivoIndireto(ofstream *arquivo_saida, int id)
+{
+    vector<int> no;
+
+    for (Node *node = this->first_node; node != nullptr; node = node->getNextNode())
+    {
+        transitivoIndireto_Aux(&no, node, id);
+    }
+
+    for (int i = 0; i < no.size(); i++)
+    {
+        *arquivo_saida << no[i] << " ";
+    }
+}
+
+void Graph::transitivoIndireto_Aux(vector<int> *no, Node *node, int id)
+{
+    for (Edge *aresta = node->getFirstEdge(); aresta != NULL; aresta = aresta->getNextEdge())
+    {
+        if (aresta->getTargetId() == id && (!std::count(no->begin(), no->end(), node->getId())))
+        {
+            no->push_back(node->getId());
+            for (Node *node = this->first_node; node != nullptr; node = node->getNextNode())
+            {
+                transitivoIndireto_Aux(no, node, node->getId());
+            }
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,109 +1,266 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
-#include <sstream>
 #include <fstream>
-#include <vector>
 #include <string>
-
-#include "Aresta.h"
+#include <math.h>
+#include <utility>
+#include <tuple>
+#include <iomanip>
+#include <stdlib.h>
+#include <chrono>
+#include "Graph.h"
+#include "Node.h"
 
 using namespace std;
 
-int main(int argc, char *argv[])
+Graph *leitura(ifstream &input_file, int directed, int weightedEdge, int weightedNode)
 {
-    string arq_entrada = argv[1];
-    string arq_saida = argv[2];
-    int Opc_Direc = stoi(argv[3]);
-    int Opc_Peso_Aresta = stoi(argv[4]);
 
-    ifstream arquivo_entrada(arq_entrada);
-    ofstream arquivo_saida(arq_saida);
+    //Variáveis para auxiliar na criação dos nós no Grafo
+    int idNodeSource;
+    int idNodeTarget;
+    int order;
 
-    if (!arquivo_entrada.is_open())
+    //Pegando a ordem do grafo
+    input_file >> order;
+
+    //Criando objeto grafo
+    Graph *graph = new Graph(order, directed, weightedEdge, weightedNode);
+
+    //Leitura de arquivo
+
+    if (!graph->getWeightedEdge() && !graph->getWeightedNode())
     {
-        cout << "Ocorreu um erro ao abrir o arquivo de entrada!";
-        exit(1);
-    }
 
-    if (!arquivo_saida.is_open())
-    {
-        cout << "Ocorreu um erro ao abrir o arquivo de saida!";
-    }
-
-    string line;
-    getline(arquivo_entrada, line);
-
-    int qntNo = stoi(line);
-    Aresta *aresta = new Aresta[qntNo + 1];
-
-    int dado1, dado2, dado3;
-    if (Opc_Peso_Aresta == 0)
-    {
-        while (arquivo_entrada >> dado1 >> dado2)
+        while (input_file >> idNodeSource >> idNodeTarget)
         {
-            aresta[dado1].adicionar(dado2);
-            if (Opc_Direc == 0)
-                aresta[dado2].adicionar(dado1);
+
+            graph->insertEdge(idNodeSource, idNodeTarget, 0);
         }
     }
-    else
+    else if (graph->getWeightedEdge() && !graph->getWeightedNode())
     {
-        while (arquivo_entrada >> dado1 >> dado2 >> dado3)
+
+        float edgeWeight;
+
+        while (input_file >> idNodeSource >> idNodeTarget >> edgeWeight)
         {
-            aresta[dado1].adicionar(dado2, dado3);
-            if (Opc_Direc == 0)
-                aresta[dado2].adicionar(dado1, dado3);
+
+            graph->insertEdge(idNodeSource, idNodeTarget, edgeWeight);
+        }
+    }
+    else if (graph->getWeightedNode() && !graph->getWeightedEdge())
+    {
+
+        float nodeSourceWeight, nodeTargetWeight;
+
+        while (input_file >> idNodeSource >> nodeSourceWeight >> idNodeTarget >> nodeTargetWeight)
+        {
+
+            graph->insertEdge(idNodeSource, idNodeTarget, 0);
+            graph->getNode(idNodeSource)->setWeight(nodeSourceWeight);
+            graph->getNode(idNodeTarget)->setWeight(nodeTargetWeight);
+        }
+    }
+    else if (graph->getWeightedNode() && graph->getWeightedEdge())
+    {
+
+        float nodeSourceWeight, nodeTargetWeight, edgeWeight;
+
+        while (input_file >> idNodeSource >> nodeSourceWeight >> idNodeTarget >> nodeTargetWeight)
+        {
+
+            graph->insertEdge(idNodeSource, idNodeTarget, edgeWeight);
+            graph->getNode(idNodeSource)->setWeight(nodeSourceWeight);
+            graph->getNode(idNodeTarget)->setWeight(nodeTargetWeight);
         }
     }
 
-    int option;
-    cout << "**********************" << endl;
-    cout << "Trabalho de Grafos" << endl;
-    cout << "----------------------" << endl;
-    cout << "1- Transitivo Direto" << endl;
-    cout << "2- Transitivo Indireto" << endl;
-    cout << "3- " << endl;
-    cout << "4- " << endl;
-    cout << "5- " << endl;
-    cout << "6- " << endl;
-    cout << "7- " << endl;
-    cout << "8- " << endl;
-    cout << "**********************" << endl;
-    cin >> option;
+    return graph;
+}
 
-    switch (option)
+Graph *leituraInstancia(ifstream &input_file, int directed, int weightedEdge, int weightedNode)
+{
+
+    //Variáveis para auxiliar na criação dos nós no Grafo
+    int idNodeSource;
+    int idNodeTarget;
+    int weight;
+    int order;
+    int numEdges;
+
+    //Pegando a ordem do grafo
+    input_file >> order >> numEdges;
+
+    //Criando objeto grafo
+    Graph *graph = new Graph(order, directed, weightedEdge, weightedNode);
+
+    //Leitura de arquivo
+    while(input_file >> idNodeSource >> idNodeTarget) {
+
+        graph->insertEdge(idNodeSource, idNodeTarget, 0);
+
+    }
+
+
+    return graph;
+}
+
+int menu()
+{
+
+    int selecao;
+
+    cout << "MENU" << endl;
+    cout << "----" << endl;
+    cout << "[1] Fecho transitivo direto deste vértice." << endl;
+    cout << "[2] Fecho transitivo indireto deste vértice." << endl;
+    cout << "[3] Caminho Mínimo entre dois vértices - Dijkstra" << endl;
+    cout << "[4] Caminho Mínimo entre dois vértices - Floyd" << endl;
+    cout << "[5] Árvore Geradora Mínima de Prim" << endl;
+    cout << "[6] Árvore Geradora Mínima de Kruskal" << endl;
+    cout << "[0] Sair" << endl;
+
+    cin >> selecao;
+
+    return selecao;
+}
+
+void selecionar(int selecao, Graph *graph, ofstream &output_file)
+{
+
+    switch (selecao)
     {
+
     case 1:
-        if (Opc_Direc == 1)
+    {
+        if (graph->getDirected() == true)
         {
             int id;
             cout << "Digite o valor de um Id do vertice: ";
             cin >> id;
-            aresta[id].transitivoDireto(&arquivo_saida, aresta);
+            graph->transitivoDireto(&output_file, id);
         }
         else
-        {
             cout << "Esté grafo não é direcionado" << endl;
-        }
-        break;
-    case 2:
-        if (Opc_Direc == 1)
-        {
-            int id;
-            cout << "Digite o valor de um Id do vertice: ";
-            cin >> id;
-            aresta[id].transitivoIndireto(&arquivo_saida, aresta, qntNo, id);
-        }
-        else
-        {
-            cout << "Esté grafo não é direcionado" << endl;
-        }
         break;
     }
 
-    delete[] aresta;
-    arquivo_saida.close();
-    arquivo_entrada.close();
+    case 2:
+    {
+        if (graph->getDirected() == true)
+        {
+            int id;
+            cout << "Digite o valor de um Id do vertice: ";
+            cin >> id;
+            graph->transitivoIndireto(&output_file, id);
+        }
+        else
+            cout << "Esté grafo não é direcionado" << endl;
+        break;
+    }
+
+    case 3:
+    {
+
+        break;
+    }
+
+    case 4:
+    {
+
+        break;
+    }
+
+    case 5:
+    {
+
+        break;
+    }
+
+    case 6:
+    {
+
+        break;
+    }
+    case 7:
+    {
+
+        break;
+    }
+    case 8:
+    {
+
+        break;
+    }
+    default:
+    {
+        cout << " Error!!! invalid option!!" << endl;
+    }
+    }
+}
+
+int mainMenu(ofstream &output_file, Graph *graph)
+{
+
+    int selecao = 1;
+
+    while (selecao != 0)
+    {
+        //system("clear");
+        selecao = menu();
+
+        if (output_file.is_open())
+            selecionar(selecao, graph, output_file);
+
+        else
+            cout << "Unable to open the output_file" << endl;
+
+        output_file << endl;
+    }
+
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+
+    //Verificação se todos os parâmetros do programa foram entrados
+    if (argc != 6)
+    {
+        cout << "ERROR: Expecting: ./execGrupo15 <arquivo_entrada> <arquivo_saida> <Opc_Direc> <Opc_Peso_Aresta> <Opc_Peso_Nos>" << endl;
+        return 1;
+    }
+
+    string program_name(argv[0]);
+    string input_file_name(argv[1]);
+
+    string instance;
+    if (input_file_name.find("v") <= input_file_name.size())
+    {
+        string instance = input_file_name.substr(input_file_name.find("v"));
+        cout << "Running " << program_name << " with instance " << instance << " ... " << endl;
+    }
+
+    //Abrindo arquivo de entrada
+    ifstream input_file;
+    ofstream output_file;
+    input_file.open(argv[1], ios::in);
+    output_file.open(argv[2], ios::out | ios::trunc);
+
+    Graph *graph;
+
+    if (input_file.is_open())
+        graph = leitura(input_file, atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+    else
+        cout << "Unable to open " << argv[1];
+
+    mainMenu(output_file, graph);
+
+    //Fechando arquivo de entrada
+    input_file.close();
+
+    //Fechando arquivo de saída
+    output_file.close();
+
     return 0;
 }
